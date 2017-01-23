f22eaf3b0a432b02ef3ce705add13d6f9eba3a40
Courtney Burns <courtneylynnburns@gmail.com>
2017-01-22 19:06:36 -0600

    added routes to new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
+  get "private_event", to: "private_event#index"
+  get "camping", to: "camping#index"
+  get "drive_in", to: "drive_in#index"
+  get "wedding", to: "wedding#index"
+  get "concert", to: "concert#index"
 end
