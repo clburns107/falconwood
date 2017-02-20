be87f95688f1061648fbebe82089cf898b5d129f
Courtney Burns <courtneylynnburns@gmail.com>
2017-02-20 16:39:39 -0600

    removed merge comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 20170114203353) do
 
   # These are extensions that must be enabled in order to support this database
