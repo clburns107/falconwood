23528bd53135c03fb7a6ff9c145af3756f80aeed
Melinda Deinert <melinda.deinert@gmail.com>
2017-02-20 22:43:19 +0000

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170114203353) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
 end
