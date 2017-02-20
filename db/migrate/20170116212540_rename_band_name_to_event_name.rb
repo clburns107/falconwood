7d0f47eaf1e69bc5ff89426b4dbafaf09e5fe976
Courtney Burns <courtneylynnburns@gmail.com>
2017-02-20 16:18:26 -0600

    rolled back db to remove all tables

--- db/migrate/20170116212540_rename_band_name_to_event_name.rb
+++
@@ -1,5 +0,0 @@
-class RenameBandNameToEventName < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :events, :band_name, :event_name
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,69 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116213104) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "bands", force: :cascade do |t|
-    t.string   "band_name"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.text     "description"
-    t.text     "notes"
-    t.string   "facebook_link"
-    t.string   "instagram_link"
-    t.string   "twitter_link"
-    t.string   "youtube_link"
-    t.string   "website_link"
-    t.text     "rider"
-    t.datetime "load_in_time"
-    t.datetime "sound_check_time"
-    t.text     "pay_agreement"
-    t.integer  "settled_amount"
-    t.integer  "event_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string   "event_name"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.text     "description"
-    t.text     "notes"
-    t.integer  "guest_number"
-    t.integer  "head_table_count"
-    t.text     "alcohol_order"
-    t.text     "table_arrangement"
-    t.text     "catering_instructions"
-    t.integer  "glamp_site_number"
-    t.string   "ceremony_site"
-    t.text     "schedule"
-    t.integer  "rental_fee"
-    t.string   "ticket_link"
-    t.string   "rating"
-    t.datetime "door_time"
-    t.datetime "presale_ticket_number"
-    t.datetime "door_ticket_number"
-  end
 
 end
