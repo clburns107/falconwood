09b4c218dfe9bcf1fb1c3b2d83de89cb65f36325
Courtney Burns <courtneylynnburns@gmail.com>
2017-02-25 16:06:43 -0600

    jquery scroll, tabs and fixed nav all work together

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,8 +1,19 @@
-
-
-//opens full screen overlay
-function scrollToElement() {
+function scrollToAbout() {
   	$('html, body').animate({
         scrollTop: $("#about").offset().top
+    }, 2000);
+}
+
+//opens full screen overlay
+function scrollToDetails() {
+  	$('html, body').animate({
+        scrollTop: $("#details").offset().top
+    }, 2000);
+}
+
+//opens full screen overlay
+function scrollToContact() {
+  	$('html, body').animate({
+        scrollTop: $("#contact").offset().top
     }, 2000);
 }
