02dae40216697c044d35bbf41255d0528acda4e2
Courtney Burns <courtneylynnburns@gmail.com>
2017-02-25 15:58:43 -0600

    jquery scroll works without altering url

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,6 +1,8 @@
-$("#tiny-nav a").on("click", function(){
-    var path = $(this).attr("data-path");
-    var anchor = $("#" + path);
-    var position = anchor.position().top;
-    $("body").animate({scrollTop: position});
-});
+
+
+//opens full screen overlay
+function scrollToElement() {
+  	$('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+}
