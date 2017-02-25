f11a0166faa379eafff1a64ec3518ff1719f44f0
Courtney Burns <courtneylynnburns@gmail.com>
2017-02-25 17:20:46 -0600

    tabs work with nav

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,13 +1,37 @@
-$(document).ready(function(){
+// $(document).ready(function(){
   
-  $('ul.tabs li').click(function(){
-    var tab_id = $(this).attr('data-tab');
+//   $('ul.tabs li').click(function(){
+//     var tab_id = $(this).attr('data-tab');
 
-    $('ul.tabs li').removeClass('current');
-    $('.tab-content').removeClass('current');
+//     $('ul.tabs li').removeClass('current');
+//     $('.tab-content').removeClass('current');
 
-    $(this).addClass('current');
-    $("#"+tab_id).addClass('current');
-  })
+//     $(this).addClass('current');
+//     $("#"+tab_id).addClass('current');
+//   })
 
-})
+// })
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
