1bfe6b9cb4a6dfffcd60b968cc74303268b11f43
Courtney Burns <courtneylynnburns@gmail.com>
2017-02-28 22:20:40 -0600

    updated gallery

---
+++ app/assets/javascripts/images.js
@@ -0,0 +1,34 @@
+// function reply_click(clicked_id)
+// {
+//     alert(clicked_id);
+// }
+
+// window.onload = function(){ 
+//     // Get the modal
+// 	var modal = document.getElementById('imgModal');
+
+// 	// Get the image and insert it inside the modal - use its "alt" text as a caption
+// 	var img = document.getElementById('myImg');
+// 	var modalImg = document.getElementById("img01");
+// 	var captionText = document.getElementById("caption");
+// 	img.onclick = function(){
+// 	    modal.style.display = "block";
+// 	    modalImg.src = this.src;
+// 	    captionText.innerHTML = this.alt;
+// 	}
+
+// 	// Get the <span> element that closes the modal
+// 	var span = document.getElementsByClassName("close")[0];
+
+// 	// When the user clicks on <span> (x), close the modal
+// 	span.onclick = function() { 
+// 	  modal.style.display = "none";
+// 	}
+// };
+
+$(function() {
+		$('.pop').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('#imagemodal').modal('show');
+		});		
+});
